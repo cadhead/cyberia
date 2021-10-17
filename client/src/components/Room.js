7ef899f05494642b740a08af01f3c4ec0799a1e2
cadhead <nekorides@gmail.com>
2021-10-17 16:20:44 +0300

    feat: playlist logic ->from client to  server

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -9,10 +9,20 @@ import { createBotMessage } from '../common/bot-message';
 const Room = ({ socket }) => {
   const roomName = window.location.pathname.split('/')[2];
   const [chatMessages, setChatMessages, sendChatMessage, addChatMessage] = useChat(socket);
-  const playlistManager = usePlaylist(socket);
+  const [
+    playlistItems,
+    currentPlaylistItem,
+    addVideo,
+    setNextPlaylistItem,
+    setCurrentPlaylistItem,
+    removePlaylistItem,
+    pinPlaylistItem,
+    setPlaylistItems
+  ] = usePlaylist(socket);
 
   function handleJoin(data) {
     setChatMessages(m => [...data.lastMessages, ...m]);
+    setPlaylistItems(p => [...p, ...data.playlistItems]);
   }
 
   function handleLeave({ /* username */ }) {
@@ -27,6 +37,10 @@ const Room = ({ socket }) => {
     addChatMessage(createBotMessage(message));
   }
 
+  function handlePlaylistAdd(item) {
+    setPlaylistItems(p => [...p, item]);
+  }
+
   useDidMount(() => {
     document.title = roomName;
     socket.emit('user:join_room', { room: roomName });
@@ -34,12 +48,22 @@ const Room = ({ socket }) => {
     socket.on('user:leave_room', handleLeave);
     socket.on('user:chat', handleChatMessage);
     socket.on('server:chat', handleChatMessageFromServer);
+    socket.on('playlist:add', handlePlaylistAdd);
   });
 
   return (
     <Fragment>
       <div className="Video Box">
-        <Video playlistManager={playlistManager} />
+        <Video socket={socket} playlistManager={[
+          playlistItems,
+          currentPlaylistItem,
+          setPlaylistItems,
+          addVideo,
+          setNextPlaylistItem,
+          setCurrentPlaylistItem,
+          removePlaylistItem,
+          pinPlaylistItem
+        ]} />
       </div>
       <div className="Chat Box">
         <Chat messages={chatMessages} sendMessage={sendChatMessage} />
--- client/src/components/Video/hooks/usePlaylist.js
+++ client/src/components/Video/hooks/usePlaylist.js
@@ -1,14 +1,6 @@
 import { useEffect, useState } from "preact/hooks";
 import { arrayMove } from "../../../common/helpers/array";
 
-const getYoutubeVideoSnippet = async (id) => {
-  const APIKey = 'AIzaSyAPHTeGKACKMAhCfuC1-utWlD4vCMnIVps';
-  const APIResponse = await (await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${APIKey}`)).json();
-  const snippet = APIResponse.items[0].snippet;
-
-  return snippet;
-}
-
 export const usePlaylist = (socket) => {
   const [playlistItems, setPlaylistItems] = useState([]);
   const [currentPlaylistItem, setCurrentPlaylistItem] = useState(playlistItems[0]);
@@ -19,24 +11,8 @@ export const usePlaylist = (socket) => {
     }
   }, [currentPlaylistItem, playlistItems]);
 
-  const addVideo = async ({
-    title, url, ytID
-  }) => {
-    if (!url.match('(http|https)://(www.)?youtube|youtu.be')) return;
-    const youtubeID = url.split(/v\/|v=|youtu\.be\//)[1].split(/[?&]/)[0];
-    if (playlistItems.find((i) => i.ytID === ytID || i.ytID === youtubeID)) return;
-
-    const info = await getYoutubeVideoSnippet(youtubeID);
-
-    setPlaylistItems([
-      ...playlistItems,
-      {
-        title: title || info.title || 'Raw video',
-        url,
-        ytID: ytID || youtubeID,
-        addedBy: window.USER.username
-      }
-    ]);
+  const addVideo = async ({ url }) => {
+    socket.emit('playlist:add', { url });
   }
 
   const setNextPlaylistItem = (item) => {
@@ -76,6 +52,7 @@ export const usePlaylist = (socket) => {
     setNextPlaylistItem,
     setCurrentPlaylistItem,
     removePlaylistItem,
-    pinPlaylistItem
+    pinPlaylistItem,
+    setPlaylistItems
   ];
 };
