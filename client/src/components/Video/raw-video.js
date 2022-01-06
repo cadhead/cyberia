36d5aa49eadaf0a34236202e14118b876f7bd4a0
cadhead <nekorides@gmail.com>
2022-01-06 09:44:17 +0300

    fix(component raw-video): sync

--- client/src/components/Video/raw-video.js
+++ client/src/components/Video/raw-video.js
@@ -1,24 +1,23 @@
 import { h } from 'preact';
 import { useEffect, useRef } from 'preact/hooks';
 
-const RawVideo = ({ item }) => {
+const RawVideo = ({ item, timeSync }) => {
   const { url } = item;
   const videoRef = useRef(null);
 
   useEffect(() => {
     const video = videoRef.current;
-    const { duration, timeLeft } = item;
+    const { duration } = item;
 
-    video.muted = true;
     video.play();
-    video.muted = false;
+    video.volume = 0.1;
 
-    if (Math.abs(item.timeLeft - video.currentTime) < 3) return;
+    if (Math.abs(timeSync - video.currentTime) < 3) return;
 
     if (duration) {
-      video.currentTime = timeLeft
+      video.currentTime = timeSync
     }
-  }, [item]);
+  }, [item, timeSync]);
 
   return (
     <video ref={videoRef} className="Video__Responsive" src={url} controls>Your browser does not support HTML5 video</video>
